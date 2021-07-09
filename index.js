a6e36aafeccd0009e0415ad8884ee10e402564ed
Benjamin <bens@spreadshirt.net>
2021-07-09 10:07:17 +0200

    convert all bash commands, but don't really call the PDF binaries

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const {
     hideBin,
 } = require('yargs/helpers'); /* hide meta args (https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/) */
 
+const { sync } = require('glob');
+
+const { copyFileSync, statSync, renameSync, unlinkSync } = require('fs');
+const { execSync } = require('child_process');
+
 // Todo
 //  Using book pages, bt don't n'up them doesn't allow different trimmings for odd/even
 //  Using book pages, bt don't halve and n'up them will print on portrait A4
@@ -17,7 +22,8 @@ function parseArguments(argsRaw) {
         .option('file-glob', {
             alias: 'f',
             type: 'string' /* array, boolean, count, number, string */,
-            description: "is the file, e.g. input.pdf or '*.pdf'",
+            description:
+                "is the file, e.g. input.pdf or '*.pdf' (sort multiple files oldest first)",
             default: 'input.pdf',
         })
         .option('scale-factor', {
@@ -55,80 +61,129 @@ function parseArguments(argsRaw) {
         .epilog("Let's go ...").argv;
 }
 
+function findFiles(glob) {
+    return sync(glob)
+        .map(fileName => {
+            return { fileName, dateModified: statSync(fileName).mtime.getTime() };
+        })
+        .sort((file1, file2) => /* oldest first */ file1.dateModified - file2.dateModified);
+}
+
+const DRY_RUN = 'dryRun';
+function execute(command, maybeDryRun) {
+    // stderr is sent to stderr of parent process (see options.stdio)
+    if (maybeDryRun === DRY_RUN) {
+        console.log(execSync(`echo '${command}'`).toString());
+    } else {
+        console.log(execSync(command).toString());
+    }
+}
+
+// PDF TOOLS
+//  mutool, pdftk, pdfjam
+//  (install binaries yourself)
 async function run() {
-    const { fileGlob, scaleFactor, halve, trim, nUp } = parseArguments(hideBin(process.argv));
-    console.log('args:', { fileGlob, scaleFactor, halve, trim, nUp });
+    const { fileGlob, halve, scaleFactor, trim, nUp } = parseArguments(hideBin(process.argv));
+    // console.log('args:', { fileGlob, scaleFactor, halve, trim, nUp });
 
-    // todo continue: start working with $file
+    findFiles(fileGlob).forEach(file => {
+        console.log(`-------------`);
+        console.log(`Starting with ${file.fileName}`);
+        console.log(`-------------`);
 
-    return 'done.';
+        // todo this is not needed I think - remove?
+        // create a copy to work with (in case we fuck up something)
+        // copyFileSync(file.fileName, 'base.pdf');
+
+        if (halve) {
+            console.log(`Splitting into half`);
+            execute(`mutool poster -y 2 ${file.fileName} halved.pdf`, DRY_RUN);
+        }
+
+        console.log('Splitting into odd/even to trim these differently');
+        if (halve) {
+            execute(`pdftk halved.pdf cat 1-endodd output odd.pdf`, DRY_RUN);
+            execute(`pdftk halved.pdf cat 2-endeven output even.pdf`, DRY_RUN);
+        } else {
+            execute(`pdftk ${file.fileName} cat 1-endodd output odd.pdf`, DRY_RUN);
+            execute(`pdftk ${file.fileName} cat 2-endeven output even.pdf`, DRY_RUN);
+        }
+
+        console.log('Scaling');
+        execute(`pdfjam --suffix scaled --scale ${scaleFactor} odd.pdf`, DRY_RUN);
+        execute(`pdfjam --suffix scaled --scale ${scaleFactor} even.pdf`, DRY_RUN);
+
+        console.log('Trimming');
+        if (nUp) {
+            // center the text
+            execute(
+                `pdfjam --suffix trimmed --trim "${trim}cm 0cm 0cm 0cm" even-scaled.pdf`,
+                DRY_RUN
+            );
+            execute(
+                `pdfjam --suffix trimmed --trim "0cm 0cm ${trim}cm 0cm" odd-scaled.pdf`,
+                DRY_RUN
+            );
+        } else {
+            // move to right to lay the hand on the tablet
+            execute(
+                `pdfjam --suffix trimmed --trim "0cm 0cm ${trim}cm 0cm" even-scaled.pdf`,
+                DRY_RUN
+            );
+            execute(
+                `pdfjam --suffix trimmed --trim "0cm 0cm ${trim}cm 0cm" odd-scaled.pdf`,
+                DRY_RUN
+            );
+        }
+
+        // create a new document
+        console.log('Combining');
+        execute(
+            `pdftk A=odd-scaled-trimmed.pdf B=even-scaled-trimmed.pdf shuffle A B output combined.pdf`,
+            DRY_RUN
+        );
+
+        // create the new output file
+        if (nUp) {
+            console.log('Creating two pages per sheet');
+            // todo originally file name was `--outfile ${file/.pdf/}_scaled.pdf`, here and in the else branch
+            //  (what does the /.pdf/ do?)
+            execute(
+                `pdfjam --nup 2x1 combined.pdf --landscape --a4paper --outfile ${
+                    file.fileName
+                }_scaled.pdf`,
+                DRY_RUN
+            );
+        } else {
+            console.log(`mv combined.pdf ${file}_scaled.pdf`);
+            // renameSync('combined.pdf', `${file.fileName}_scaled.pdf`);
+        }
+
+        console.log('Cleaning up');
+        [
+            'base.pdf',
+            'input-scaled.pdf',
+            'halved.pdf',
+            'odd.pdf',
+            'odd-scaled.pdf',
+            'odd-scaled-trimmed.pdf',
+            'even.pdf',
+            'even-scaled.pdf',
+            'even-scaled-trimmed.pdf',
+            'combined.pdf',
+        ].forEach(tmpFile => {
+            unlinkSync(tmpFile);
+        });
+    });
+
+    return `
+
+
+
+Done. Created a new document with scale factor ${scaleFactor} and move pages ${trim}cm to the fold line...
+Now scroll through your document to check if the settings are fine...`;
 }
 
 run()
     .then(console.log)
     .catch(console.error);
-
-// ...
-// find . -name "$file" -print0 | xargs -0 ls -tr | while read file;
-// do
-//     echo "-------------"
-// echo "Starting with $file"
-// echo "-------------"
-//
-// # create a copy to work with
-//     cp $file base.pdf
-//
-// # halve book scans
-// if [ $halve = 1 ]; then
-// echo "Splitting into half"
-// mutool poster -y 2 $file halved.pdf
-// fi
-//
-// # split odd and even pages
-// # just do it to simplify the code (even when not needed, e.g. for a simple "Scale A4 and move left" example)
-// echo "Splitting into odd/even to trim these differently"
-// if [ $halve = 1 ]; then
-// pdftk halved.pdf cat 1-endodd output odd.pdf
-// pdftk halved.pdf cat 2-endeven output even.pdf
-// else
-// pdftk $file cat 1-endodd output odd.pdf
-// pdftk $file cat 2-endeven output even.pdf
-// fi
-//
-// # scale the pages
-// echo "Scaling"
-// pdfjam --suffix scaled --scale ${scale} odd.pdf
-// pdfjam --suffix scaled --scale ${scale} even.pdf
-//
-// #trim the pages
-// echo "Trimming"
-// if [ $nup = 1 ]; then #center the text
-// pdfjam --suffix trimmed --trim "${trim}cm 0cm 0cm 0cm" even-scaled.pdf
-// pdfjam --suffix trimmed --trim "0cm 0cm ${trim}cm 0cm" odd-scaled.pdf
-// else # move to right to lay the hand on the tablet
-// pdfjam --suffix trimmed --trim "0cm 0cm ${trim}cm 0cm" even-scaled.pdf
-// pdfjam --suffix trimmed --trim "0cm 0cm ${trim}cm 0cm" odd-scaled.pdf
-// fi
-//
-// # create a new document
-// echo "Combining"
-// pdftk A=odd-scaled-trimmed.pdf B=even-scaled-trimmed.pdf shuffle A B output combined.pdf
-//
-// # create the new output file
-// if [ $nup = 1 ]; then
-// echo "Creating two pages per sheet"
-// pdfjam --nup 2x1 combined.pdf --landscape --a4paper --outfile ${file/.pdf/}_scaled.pdf
-// else
-// mv combined.pdf ${file/.pdf/}_scaled.pdf
-// fi
-//
-// echo "Cleaning up"
-// rm base.pdf input-scaled.pdf halved.pdf odd.pdf odd-scaled.pdf odd-scaled-trimmed.pdf even.pdf even-scaled.pdf even-scaled-trimmed.pdf combined.pdf
-//
-// done
-//
-// echo ""
-// echo ""
-// echo ""
-// echo "Done. Created a new document with scale factor \"${scale}\" and move pages \"${trim}cm\" to the foldline..."
-// echo "Now scroll through your document to check if the settings are fine..."
